9a4788caa6b16773f531a3ecd219214435ab38cd
cuilei5205189 <allen5205189@gmail.com>
2018-07-23 21:13:33 +0800

    init开发环境设置

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp')
+jade = require('gulp-jade')
+sass = require('gulp-sass')
+plumber = require('gulp-plumber')
+
+
+gulp.task('images', function () {
+    return gulp.src('images/**').pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('sass', function () {
+    return gulp.src('sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('jade', function () {
+    return gulp.src('jade/*.jade')
+        .pipe(plumber())
+        .pipe(jade())
+        .pipe(gulp.dest('dist'))
+})
+
+gulp.task('see', function () {
+    gulp.watch('sass/**', ['sass'])
+    gulp.watch('jade/*.jade', ['jade'])
+})
+
+gulp.task('default', ['see']);
